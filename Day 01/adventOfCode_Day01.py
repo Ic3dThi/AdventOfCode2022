a4c8bc3024d0c277c5cfeaad7385c9e18a0f43cf
Thi Tran <torghiria@gmail.com>
2022-12-01 21:19:58 -0600

    Organized it

---
+++ Day 01/adventOfCode_Day01.py
@@ -0,0 +1,14 @@
+#Advent of Code 2022 12 01
+#Author: ya boi
+inputFile = open("adventOfCodeInput_Day01.txt", 'r')
+inputList = inputFile.readlines()
+max = 0
+curr = 0
+for line in inputList:
+    if line == "\n":
+        curr = 0
+    else:
+        curr += int(line)
+    if curr > max:
+        max = curr
+print(max)
